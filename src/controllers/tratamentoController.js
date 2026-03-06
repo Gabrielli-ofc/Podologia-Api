e8de35b33a236ca9f08b2f2778d73776a8f06fe2
Gabrielli-ofc <frivoligabrielli@gmail.com>
2026-03-06 17:08:01 -0300

    fizemos o alterações de adição no "Joi" do tipo e valor no tratamento.controller

--- src/controllers/tratamentoController.js
+++ src/controllers/tratamentoController.js
@@ -3,22 +3,20 @@ import Joi from 'joi';
 
 export const tratamentoCreateSchema = Joi.object({
     tipo: Joi.string().length(11).required(),
-    valor: Joi.string().required(),
-    : Joi.string().required(),
+    valor: Joi.number().positive().required()
 });
 
 export const tratamentoUpdateSchema = Joi.object({
     tipo: Joi.string(),
-    valor: Joi.string(),
-    : Joi.string(),
-}).min(1);
+    valor: Joi.number().positive(),
+});
 
 export const listarTratamento = async (req, res) => {
     try {
-    const agendamento = await agendamentoService.findAll();
-    res.status(200).json(agendamento);
+    const tratamento = await tratamentoService.findAll(req.body);
+    res.status(200).json(tratamento);
 } catch (err) {
-    console.error('Erro ao buscar agendamento:', err);
+    console.error('Erro ao buscar tratamento:', err);
     res.status(500).json({ error: 'Erro interno do servidor' });
 }
 };
@@ -37,31 +35,31 @@ export const adicionarTratamento= async (req, res) => {
         }
     };
 
-    export const atualizarAgendamento = async (req, res) => {
+    export const atualizarTratamento = async (req, res) => {
             try {
                 const { cpf } = req.params;
-                const update = await agendamentoService.update(cpf, req.body);
+                const update = await tratamentoService.update(cpf, req.body);
                 if (!update) {
-                    return res.status(404).json({ error: 'Agendamento não encontrado' });
+                    return res.status(404).json({ error: 'tratamento não encontrado' });
                 }
-                res.status(200).json({ message: 'Agendamento atualizado com sucesso'})
+                res.status(200).json({ message: 'tratamento atualizado com sucesso'})
                 } catch (err) {
-                    console.error('Erro ao atualizar agendamento:', err);
-                    res.status(500).json({ error: 'Erro ao atualizar agendamento' })
+                    console.error('Erro ao atualizar tratamento:', err);
+                    res.status(500).json({ error: 'Erro ao atualizar tratamento' })
                 }
         };
     
-         export const deletarAgendamento = async (req, res) => {
+         export const deletarTratamento = async (req, res) => {
                 try {
                     const { cpf } = req.params;
-                    const deleted = await agendamentoService.remove(cpf);
+                    const deleted = await tratamentoService.remove(cpf);
                     if (!deleted) {
-                        return res.status(404).json({ error: 'Agendamento não encontrado com sucesso' });
+                        return res.status(404).json({ error: 'tratamento não encontrado com sucesso' });
                    }
-                   res.status(200).json({ message: 'Agendamento deletado com sucesso' });
+                   res.status(200).json({ message: 'tratamento deletado com sucesso' });
             } catch (err) {
-                console.error('Erro ao deletar agendamento:', err);
-                res.status(500).json({ error: 'Erro ao deletar agendamento'});
+                console.error('Erro ao deletar tratamento:', err);
+                res.status(500).json({ error: 'Erro ao deletar tratamento'});
             }
             };
         
